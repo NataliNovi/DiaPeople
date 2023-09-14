d8c00ef8990fb04ff75fb29b037c1a791ebb3b10
Natalia Novikova <106342111+NataliNovi@users.noreply.github.com>
2023-09-14 12:52:21 +0300

    about insulin

---
+++ assets/js/insulin.js
@@ -0,0 +1,49 @@
+//стр инсулин кнопка читать статью
+
+function myFunctionReadArticleIns1() {
+  var dotsReadArticleIns1 = document.getElementById("dots-read-article-ins1");
+  var moreReadArticleIns1 = document.getElementById("read-more-article-ins1");
+  var btnReadArticleIns1 = document.getElementById("myBtnReadArticleIns1");
+
+  if (dotsReadArticleIns1.style.display === "none") {
+    dotsReadArticleIns1.style.display = "inline";
+    btnReadArticleIns1.innerHTML = "Читать";
+    moreReadArticleIns1.style.display = "none";
+  } else {
+    dotsReadArticleIns1.style.display = "none";
+    btnReadArticleIns1.innerHTML = "Свернуть";
+    moreReadArticleIns1.style.display = "inline";
+  }
+}
+
+function myFunctionReadArticleIns2() {
+  var dotsReadArticleIns2 = document.getElementById("dots-read-article-ins2");
+  var moreReadArticleIns2 = document.getElementById("read-more-article-ins2");
+  var btnReadArticleIns2 = document.getElementById("myBtnReadArticleIns2");
+
+  if (dotsReadArticleIns2.style.display === "none") {
+    dotsReadArticleIns2.style.display = "inline";
+    btnReadArticleIns2.innerHTML = "Читать";
+    moreReadArticleIns2.style.display = "none";
+  } else {
+    dotsReadArticleIns2.style.display = "none";
+    btnReadArticleIns2.innerHTML = "Свернуть";
+    moreReadArticleIns2.style.display = "inline";
+  }
+}
+
+function myFunctionReadArticleIns3() {
+  var dotsReadArticleIns3 = document.getElementById("dots-read-article-ins3");
+  var moreReadArticleIns3 = document.getElementById("read-more-article-ins3");
+  var btnReadArticleIns3 = document.getElementById("myBtnReadArticleIns3");
+
+  if (dotsReadArticleIns3.style.display === "none") {
+    dotsReadArticleIns3.style.display = "inline";
+    btnReadArticleIns3.innerHTML = "Читать";
+    moreReadArticleIns3.style.display = "none";
+  } else {
+    dotsReadArticleIns3.style.display = "none";
+    btnReadArticleIns3.innerHTML = "Свернуть";
+    moreReadArticleIns3.style.display = "inline";
+  }
+}
---
+++ assets/js/psychology.js
@@ -0,0 +1,97 @@
+//стр психолога кнопка читать статью
+
+function myFunctionReadArticle1() {
+  var dotsReadArticle1 = document.getElementById("dots-read-article1");
+  var moreReadArticle1 = document.getElementById("read-more-article1");
+  var btnReadArticle1 = document.getElementById("myBtnReadArticle1");
+
+  if (dotsReadArticle1.style.display === "none") {
+    dotsReadArticle1.style.display = "inline";
+    btnReadArticle1.innerHTML = "Читать";
+    moreReadArticle1.style.display = "none";
+  } else {
+    dotsReadArticle1.style.display = "none";
+    btnReadArticle1.innerHTML = "Свернуть";
+    moreReadArticle1.style.display = "inline";
+  }
+}
+
+function myFunctionReadArticle2() {
+  var dotsReadArticle2 = document.getElementById("dots-read-article2");
+  var moreReadArticle2 = document.getElementById("read-more-article2");
+  var btnReadArticle2 = document.getElementById("myBtnReadArticle2");
+
+  if (dotsReadArticle2.style.display === "none") {
+    dotsReadArticle2.style.display = "inline";
+    btnReadArticle2.innerHTML = "Читать";
+    moreReadArticle2.style.display = "none";
+  } else {
+    dotsReadArticle2.style.display = "none";
+    btnReadArticle2.innerHTML = "Свернуть";
+    moreReadArticle2.style.display = "inline";
+  }
+}
+
+function myFunctionReadArticle3() {
+  var dotsReadArticle3 = document.getElementById("dots-read-article3");
+  var moreReadArticle3 = document.getElementById("read-more-article3");
+  var btnReadArticle3 = document.getElementById("myBtnReadArticle3");
+
+  if (dotsReadArticle3.style.display === "none") {
+    dotsReadArticle3.style.display = "inline";
+    btnReadArticle3.innerHTML = "Читать";
+    moreReadArticle3.style.display = "none";
+  } else {
+    dotsReadArticle3.style.display = "none";
+    btnReadArticle3.innerHTML = "Свернуть";
+    moreReadArticle3.style.display = "inline";
+  }
+}
+
+function myFunctionReadArticle4() {
+  let dotsReadArticle4 = document.getElementById("dots-read-article4");
+  let moreReadArticle4 = document.getElementById("read-more-article4");
+  let btnReadArticle4 = document.getElementById("myBtnReadArticle4");
+
+  if (dotsReadArticle4.style.display === "none") {
+    dotsReadArticle4.style.display = "inline";
+    btnReadArticle4.innerHTML = "Читать";
+    moreReadArticle4.style.display = "none";
+  } else {
+    dotsReadArticle4.style.display = "none";
+    btnReadArticle4.innerHTML = "Свернуть";
+    moreReadArticle4.style.display = "inline";
+  }
+}
+
+function myFunctionReadArticle5() {
+  let dotsReadArticle5 = document.getElementById("dots-read-article5");
+  let moreReadArticle5 = document.getElementById("read-more-article5");
+  let btnReadArticle5 = document.getElementById("myBtnReadArticle5");
+
+  if (dotsReadArticle5.style.display === "none") {
+    dotsReadArticle5.style.display = "inline";
+    btnReadArticle5.innerHTML = "Читать";
+    moreReadArticle5.style.display = "none";
+  } else {
+    dotsReadArticle5.style.display = "none";
+    btnReadArticle5.innerHTML = "Свернуть";
+    moreReadArticle5.style.display = "inline";
+  }
+}
+
+function myFunctionReadArticle6() {
+  let dotsReadArticle6 = document.getElementById("dots-read-article6");
+  let moreReadArticle6 = document.getElementById("read-more-article6");
+  let btnReadArticle6 = document.getElementById("myBtnReadArticle6");
+
+  if (dotsReadArticle6.style.display === "none") {
+    dotsReadArticle6.style.display = "inline";
+    btnReadArticle6.innerHTML = "Читать";
+    moreReadArticle6.style.display = "none";
+  } else {
+    dotsReadArticle6.style.display = "none";
+    btnReadArticle6.innerHTML = "Свернуть";
+    moreReadArticle6.style.display = "inline";
+  }
+}
