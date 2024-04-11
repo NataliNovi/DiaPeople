da65512e244f5b3a14698678be36c4bc567b23ad
Natalia Novikova <106342111+NataliNovi@users.noreply.github.com>
2024-04-11 12:57:09 +0300

    celiakia

--- assets/js/celiakia.js
+++ assets/js/celiakia.js
@@ -19,3 +19,135 @@ function myFunctionReadArticleCeliakia1() {
     moreReadArticleCeliakia1.style.display = "inline";
   }
 }
+
+function myFunctionReadArticleCeliakiaGarden() {
+  var dotsReadArticleCeliakiaGarden = document.getElementById(
+    "dots-read-articleCeliakiaGarden"
+  );
+  var moreReadArticleCeliakiaGarden = document.getElementById(
+    "read-more-articleCeliakiaGarden"
+  );
+  var btnReadArticleCeliakiaGarden = document.getElementById(
+    "myBtnReadArticleCeliakiaGarden"
+  );
+
+  if (dotsReadArticleCeliakiaGarden.style.display === "none") {
+    dotsReadArticleCeliakiaGarden.style.display = "inline";
+    btnReadArticleCeliakiaGarden.innerHTML = "Читать";
+    moreReadArticleCeliakiaGarden.style.display = "none";
+  } else {
+    dotsReadArticleCeliakiaGarden.style.display = "none";
+    btnReadArticleCeliakiaGarden.innerHTML = "Свернуть";
+    moreReadArticleCeliakiaGarden.style.display = "inline";
+  }
+}
+
+function myFunctionReadArticleCeliakiaSchool() {
+  var dotsReadArticleCeliakiaSchool = document.getElementById(
+    "dots-read-articleCeliakiaSchool"
+  );
+  var moreReadArticleCeliakiaSchool = document.getElementById(
+    "read-more-articleCeliakiaSchool"
+  );
+  var btnReadArticleCeliakiaSchool = document.getElementById(
+    "myBtnReadArticleCeliakiaSchool"
+  );
+
+  if (dotsReadArticleCeliakiaSchool.style.display === "none") {
+    dotsReadArticleCeliakiaSchool.style.display = "inline";
+    btnReadArticleCeliakiaSchool.innerHTML = "Читать";
+    moreReadArticleCeliakiaSchool.style.display = "none";
+  } else {
+    dotsReadArticleCeliakiaSchool.style.display = "none";
+    btnReadArticleCeliakiaSchool.innerHTML = "Свернуть";
+    moreReadArticleCeliakiaSchool.style.display = "inline";
+  }
+}
+
+function myFunctionReadArticleCeliakiaPamyatka() {
+  var dotsReadArticleCeliakiaPamyatka = document.getElementById(
+    "dots-read-articleCeliakiaPamyatka"
+  );
+  var moreReadArticleCeliakiaPamyatka = document.getElementById(
+    "read-more-articleCeliakiaPamyatka"
+  );
+  var btnReadArticleCeliakiaPamyatka = document.getElementById(
+    "myBtnReadArticleCeliakiaPamyatka"
+  );
+
+  if (dotsReadArticleCeliakiaPamyatka.style.display === "none") {
+    dotsReadArticleCeliakiaPamyatka.style.display = "inline";
+    btnReadArticleCeliakiaPamyatka.innerHTML = "Читать";
+    moreReadArticleCeliakiaPamyatka.style.display = "none";
+  } else {
+    dotsReadArticleCeliakiaPamyatka.style.display = "none";
+    btnReadArticleCeliakiaPamyatka.innerHTML = "Свернуть";
+    moreReadArticleCeliakiaPamyatka.style.display = "inline";
+  }
+}
+
+function myFunctionReadArticleCeliakiaRestoran() {
+  var dotsReadArticleCeliakiaRestoran = document.getElementById(
+    "dots-read-articleCeliakiaRestoran"
+  );
+  var moreReadArticleCeliakiaRestoran = document.getElementById(
+    "read-more-articleCeliakiaRestoran"
+  );
+  var btnReadArticleCeliakiaRestoran = document.getElementById(
+    "myBtnReadArticleCeliakiaRestoran"
+  );
+
+  if (dotsReadArticleCeliakiaRestoran.style.display === "none") {
+    dotsReadArticleCeliakiaRestoran.style.display = "inline";
+    btnReadArticleCeliakiaRestoran.innerHTML = "Читать";
+    moreReadArticleCeliakiaRestoran.style.display = "none";
+  } else {
+    dotsReadArticleCeliakiaRestoran.style.display = "none";
+    btnReadArticleCeliakiaRestoran.innerHTML = "Свернуть";
+    moreReadArticleCeliakiaRestoran.style.display = "inline";
+  }
+}
+
+function myFunctionReadArticleCeliakiaBezgluten() {
+  var dotsReadArticleCeliakiaBezgluten = document.getElementById(
+    "dots-read-articleCeliakiaBezgluten"
+  );
+  var moreReadArticleCeliakiaBezgluten = document.getElementById(
+    "read-more-articleCeliakiaBezgluten"
+  );
+  var btnReadArticleCeliakiaBezgluten = document.getElementById(
+    "myBtnReadArticleCeliakiaBezgluten"
+  );
+
+  if (dotsReadArticleCeliakiaBezgluten.style.display === "none") {
+    dotsReadArticleCeliakiaBezgluten.style.display = "inline";
+    btnReadArticleCeliakiaBezgluten.innerHTML = "Читать";
+    moreReadArticleCeliakiaBezgluten.style.display = "none";
+  } else {
+    dotsReadArticleCeliakiaBezgluten.style.display = "none";
+    btnReadArticleCeliakiaBezgluten.innerHTML = "Свернуть";
+    moreReadArticleCeliakiaBezgluten.style.display = "inline";
+  }
+}
+
+function myFunctionReadArticleCeliakiaSkin() {
+  var dotsReadArticleCeliakiaSkin = document.getElementById(
+    "dots-read-articleCeliakiaSkin"
+  );
+  var moreReadArticleCeliakiaSkin = document.getElementById(
+    "read-more-articleCeliakiaSkin"
+  );
+  var btnReadArticleCeliakiaSkin = document.getElementById(
+    "myBtnReadArticleCeliakiaSkin"
+  );
+
+  if (dotsReadArticleCeliakiaSkin.style.display === "none") {
+    dotsReadArticleCeliakiaSkin.style.display = "inline";
+    btnReadArticleCeliakiaSkin.innerHTML = "Читать";
+    moreReadArticleCeliakiaSkin.style.display = "none";
+  } else {
+    dotsReadArticleCeliakiaSkin.style.display = "none";
+    btnReadArticleCeliakiaSkin.innerHTML = "Свернуть";
+    moreReadArticleCeliakiaSkin.style.display = "inline";
+  }
+}
