4c5e705574b2729e07583b9d1c7ad3a4d1752413
Natalia Novikova <106342111+NataliNovi@users.noreply.github.com>
2024-05-27 09:43:19 +0300

    cookie  version with first time visit

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,49 +1,37 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var consentContainer = document.getElementById("cookieConsentContainer");
-  var acceptBtn = document.getElementById("acceptCookieConsent");
-  var declineBtn = document.getElementById("declineCookieConsent");
-  var settingsBtn = document.getElementById("settingsCookieConsent");
-
-  function showConsentContainer() {
-    if (!getCookie("cookieConsent")) {
-      consentContainer.style.display = "block";
-    }
+window.onload = function () {
+  if (!getCookie("cookieConsent")) {
+    document.getElementById("cookieConsent").style.display = "block";
   }
+};
 
-  acceptBtn.onclick = function () {
-    setCookie("cookieConsent", "accept", 365);
-    consentContainer.style.display = "none";
-  };
+document.getElementById("acceptCookies").onclick = function () {
+  setCookie("cookieConsent", "accepted", 365);
+  document.getElementById("cookieConsent").style.display = "none";
+};
 
-  declineBtn.onclick = function () {
-    setCookie("cookieConsent", "decline", 365);
-    consentContainer.style.display = "none";
-  };
+document.getElementById("rejectCookies").onclick = function () {
+  setCookie("cookieConsent", "rejected", 365);
+  document.getElementById("cookieConsent").style.display = "none";
+};
 
-  settingsBtn.onclick = function () {
-    window.location.href = "..//pages/cookie-settings.html"; // Ссылка на вашу страницу настроек cookies
-  };
+document.getElementById("cookieSettings").onclick = function () {
+  window.location.href = "..//pages/cookie-settings.html"; // Ссылка на страницу настроек
+};
 
-  function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
-
-  function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == " ") c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-  }
-
-  showConsentContainer();
-});
+  return null;
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -7211,3 +7211,41 @@ var swiper = new Swiper(".mySwiper", {
   mousewheel: true,
   keyboard: true,
 });
+
+window.onload = function () {
+  if (!getCookie("cookieConsent")) {
+    document.getElementById("cookieConsent").style.display = "block";
+  }
+};
+
+document.getElementById("acceptCookies").onclick = function () {
+  setCookie("cookieConsent", "accepted", 365);
+  document.getElementById("cookieConsent").style.display = "none";
+};
+
+document.getElementById("rejectCookies").onclick = function () {
+  setCookie("cookieConsent", "rejected", 365);
+  document.getElementById("cookieConsent").style.display = "none";
+};
+
+document.getElementById("cookieSettings").onclick = function () {
+  window.location.href = "..//pages/cookie-settings.html"; // Ссылка на страницу настроек
+};
+
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
